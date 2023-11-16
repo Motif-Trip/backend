048d85d98ab7ca4d571486abadd80be7447f3538
Thingk0 <audtjd9080@naver.com>
2023-11-16 14:51:40 +0900

    :sparkles: 로그인 요청/응답용 DTO 추가.
    - [X] 두 DTO 는 기능상 차이는 없지만, 이름을 통해 프로세스의 흐름을 명확히 하기 위해서 생성.

---
+++ src/main/java/com/ssafy/motif/app/dto/member/LoginRequestDto.java
@@ -0,0 +1,21 @@
+package com.ssafy.motif.app.dto.member;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginRequestDto {
+
+    private String email;
+    private String password;
+
+}
---
+++ src/main/java/com/ssafy/motif/app/dto/member/LoginResponseDto.java
@@ -0,0 +1,19 @@
+package com.ssafy.motif.app.dto.member;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginResponseDto {
+
+    private String email;
+    private String password;
+
+}
