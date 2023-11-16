6c9df4d1d1abbbaa4975388442741f22df1865aa
Thingk0 <audtjd9080@naver.com>
2023-11-16 14:54:59 +0900

    :recycle: Post 컨트롤러 코드 포맷팅.

--- src/main/java/com/ssafy/motif/app/controller/PostController.java
+++ src/main/java/com/ssafy/motif/app/controller/PostController.java
@@ -17,39 +17,38 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @RequestMapping("/posts/api")
 public class PostController {
-	private final PostService postService;
-	
-	@PostMapping("/write")
-	public ResponseEntity<?> postWrite(Post post){
-		postService.postWrite(post);
-		
-		return new ResponseEntity<>(HttpStatus.OK);
-	}
-	
-	@GetMapping("/list")
-	public ResponseEntity<?> postList(){
-		return ResponseEntity.ok(postService.postList());
-	}
-	
-	@PostMapping("/select/{postId}")
-	public ResponseEntity<?> postSelect(@PathVariable Long postId){
-		return ResponseEntity.ok(postService.postSelect(postId));
-		
-	}
-	
-	@PostMapping("/modify")
-	public ResponseEntity<?> postModify(Post post){
-		postService.postModify(post);
-		
-		return new ResponseEntity<>(HttpStatus.OK);
-		
-	}
-	
-	@PostMapping("/delete/{postId}")
-	public ResponseEntity<?> postDelete(@PathVariable Long postId){
-		postService.postDelete(postId);
-		
-		return new ResponseEntity<>(HttpStatus.OK);	
-	}
+
+    private final PostService postService;
+
+    @PostMapping("/write")
+    public ResponseEntity<?> postWrite(Post post) {
+        postService.postWrite(post);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<?> postList() {
+        return ResponseEntity.ok(postService.postList());
+    }
+
+
+    @PostMapping("/select/{postId}")
+    public ResponseEntity<?> postSelect(@PathVariable Long postId) {
+        return ResponseEntity.ok(postService.postSelect(postId));
+
+    }
+
+    @PostMapping("/modify")
+    public ResponseEntity<?> postModify(Post post) {
+        postService.postModify(post);
+        return new ResponseEntity<>(HttpStatus.OK);
+
+    }
+
+    @PostMapping("/delete/{postId}")
+    public ResponseEntity<?> postDelete(@PathVariable Long postId) {
+        postService.postDelete(postId);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 
 }
