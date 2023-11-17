bc0c92e5f3189bd97f52a012dace74a72546c01c
Thingk0 <audtjd9080@naver.com>
2023-11-17 16:22:09 +0900

    :sparkles: 게시물 관련 API 주소 변경 및 생성 기능 수정
    - [X] URL 구조 통일.
    - [X] 게시물 요청 DTO와 인증 객체를 서비스 로직으로 전달하여 생성할 수 있도록 구현.

--- src/main/java/com/ssafy/motif/app/controller/PostController.java
+++ src/main/java/com/ssafy/motif/app/controller/PostController.java
@@ -1,57 +1,60 @@
 package com.ssafy.motif.app.controller;
 
+import com.ssafy.motif.app.dto.post.PostRequestDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ssafy.motif.app.dto.post.Post;
 import com.ssafy.motif.app.service.PostService;
 
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/posts/api")
+@RequestMapping("/api/v1/posts")
 public class PostController {
-	private final PostService postService;
-	
-	@PostMapping("/write")
-	public ResponseEntity<?> postWrite(Post post){
-		postService.postWrite(post);
-		
-		return new ResponseEntity<>(post, HttpStatus.CREATED);
-	}
-	
-	@GetMapping("/list")
-	public ResponseEntity<?> postList(){
-		return ResponseEntity.ok(postService.postList());
-	}
-	
-	@GetMapping("/select/{postId}")
-	public ResponseEntity<?> postSelect(@PathVariable Long postId){
-		return ResponseEntity.ok(postService.postSelect(postId));
-		
-	}
-	
-	@PutMapping("/modify")
-	public ResponseEntity<?> postModify(Post post){
-		postService.postModify(post);
-		
-		return new ResponseEntity<>(HttpStatus.OK);
-		
-	}
-	
-	@DeleteMapping("/delete/{postId}")
-	public ResponseEntity<?> postDelete(@PathVariable Long postId){
-		postService.postDelete(postId);
-		
-		return new ResponseEntity<>(HttpStatus.NO_CONTENT);	
-	}
+
+    private final PostService postService;
+
+    @PostMapping
+    public ResponseEntity<?> create(
+        @RequestBody PostRequestDto requestDto,
+        Authentication authentication
+    ) {
+        postService.create(requestDto, authentication.getName());
+        return ResponseEntity.status(HttpStatus.CREATED).body(requestDto);
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<?> postList() {
+        return ResponseEntity.ok(postService.postList());
+    }
+
+    @GetMapping("/select/{postId}")
+    public ResponseEntity<?> postSelect(@PathVariable Long postId) {
+        return ResponseEntity.ok(postService.postSelect(postId));
+
+    }
+
+    @PutMapping("/modify")
+    public ResponseEntity<?> postModify(PostRequestDto postRequestDto) {
+        postService.postModify(postRequestDto);
+        return new ResponseEntity<>(HttpStatus.OK);
+
+    }
+
+    @DeleteMapping("/delete/{postId}")
+    public ResponseEntity<?> postDelete(@PathVariable Long postId) {
+        postService.postDelete(postId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 
 }
