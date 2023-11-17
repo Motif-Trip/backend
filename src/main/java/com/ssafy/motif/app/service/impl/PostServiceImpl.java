1e1a43c4bf13f538a5a5d7094ea346d9a0b06bba
Thingk0 <audtjd9080@naver.com>
2023-11-17 16:20:53 +0900

    :sparkles: Post 생성 로직 수정.
    - [X] Authentication 으로 부터 email 을 반환받아 회원ID 를 조회하고 이를 함께 Post 를 저장하도록 변경.

--- src/main/java/com/ssafy/motif/app/service/impl/PostServiceImpl.java
+++ src/main/java/com/ssafy/motif/app/service/impl/PostServiceImpl.java
@@ -1,49 +1,62 @@
 package com.ssafy.motif.app.service.impl;
 
+import com.ssafy.motif.app.dto.member.LoginResponseDto;
+import com.ssafy.motif.app.dto.member.MemberResponseDto;
+import com.ssafy.motif.app.dto.post.PostRequestDto;
+import com.ssafy.motif.app.mapper.MemberMapper;
 import java.util.List;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import com.ssafy.motif.app.dto.post.Post;
 import com.ssafy.motif.app.mapper.PostMapper;
 import com.ssafy.motif.app.service.PostService;
 
 import lombok.RequiredArgsConstructor;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
-public class PostServiceImpl implements PostService{
-	private final PostMapper postMapper;
-	
-	@Override
-	public void postWrite(Post post) {
-		postMapper.postWrite(post);
-		// TODO Auto-generated method stub
-		
-	}
+public class PostServiceImpl implements PostService {
 
-	@Override
-	public List<Post> postList() {
-		return postMapper.postList();
-	}
+    private final PostMapper postMapper;
+    private final MemberMapper memberMapper;
 
-	@Override
-	public Post postSelect(Long postId) {
-		// TODO Auto-generated method stub
-		return postMapper.postSelect(postId);
-	}
+    @Override
+    public void create(PostRequestDto requestDto, String email) {
+        /* 이메일을 통해 회원 ID 조회 */
+        Long memberId = memberMapper.findByEmail(email)
+            .map(LoginResponseDto::getMemberId)
+            .orElseThrow(() -> new IllegalArgumentException("이메일을 통한 회원 조회 실패 : " + email));
 
-	@Override
-	public void postModify(Post post) {
-		// TODO Auto-generated method stub
-		postMapper.postModify(post);
-		
-	}
+        /* 게시물 생성 */
+        postMapper.save(requestDto, memberId);
+        /* 생성이 됐다면, postId 에 키값이 담길 것.*/
+        log.debug("게시물 생성 성공 : {}", requestDto);
+    }
 
-	@Override
-	public void postDelete(Long postId) {
-		// TODO Auto-generated method stub
-		postMapper.postDelete(postId);
-	}
-	
+    @Override
+    public List<PostRequestDto> postList() {
+        return postMapper.postList();
+    }
+
+    @Override
+    public PostRequestDto postSelect(Long postId) {
+        // TODO Auto-generated method stub
+        return postMapper.postSelect(postId);
+    }
+
+    @Override
+    public void postModify(PostRequestDto postRequestDto) {
+        // TODO Auto-generated method stub
+        postMapper.postModify(postRequestDto);
+
+    }
+
+    @Override
+    public void postDelete(Long postId) {
+        // TODO Auto-generated method stub
+        postMapper.postDelete(postId);
+    }
+
 }
