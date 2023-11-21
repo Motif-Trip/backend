ba8c3afa6453370db0c4be6ac48b99847fd82ced
azin Lee <nuestiloveyou@gmail.com>
2023-11-21 11:31:10 +0900

    :sparkles: Image api developed.

--- src/main/java/com/ssafy/motif/app/controller/ImageController.java
+++ src/main/java/com/ssafy/motif/app/controller/ImageController.java
@@ -1,33 +1,42 @@
 package com.ssafy.motif.app.controller;
 
-import java.io.IOException;
-import java.util.UUID;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.ssafy.motif.app.dto.image.ProfileImage;
 import com.ssafy.motif.app.service.ImageService;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/images/api")
+@RequestMapping("/api/v1/profileImages")
 public class ImageController {
+	
 	private final ImageService imageService;
 	
 	// 프사 등록
-	@PostMapping("/profilepic/{memberId}")
-	public ResponseEntity<?> profilePicAdd(MultipartFile file, @PathVariable Long memberId){
-		imageService.profilePicAdd(file, memberId);
+	@PostMapping
+	public ResponseEntity<?> profilePicAdd(MultipartFile file, Authentication authentication){
+		log.info(file.getOriginalFilename());
 		
-		return new ResponseEntity<>(memberId, HttpStatus.CREATED);
+		imageService.profilePicAdd(file, authentication.getName());
+		
+		return new ResponseEntity<>(file.getOriginalFilename(), HttpStatus.CREATED);
+	}
+	
+	// 프사 불러오기(BufferedImage)
+	@GetMapping
+	public ResponseEntity<?> profilePicLoad(@PathVariable Authentication authentication){
+		return new ResponseEntity<>(imageService.profilePicLoad(authentication.getName()), HttpStatus.OK);
 	}
 	
 }
