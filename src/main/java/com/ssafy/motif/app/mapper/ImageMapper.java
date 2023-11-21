752ac996544cd0051ba0c4953bce59d29e5e5c56
azin Lee <nuestiloveyou@gmail.com>
2023-11-21 11:57:34 +0900

    :sparkles: image dto&mapper developed

---
+++ src/main/java/com/ssafy/motif/app/mapper/ImageMapper.java
@@ -0,0 +1,8 @@
+package com.ssafy.motif.app.mapper;
+
+import com.ssafy.motif.app.dto.image.ProfileImage;
+
+public interface ImageMapper {
+	void profilePicAdd(ProfileImage profileImage);
+	ProfileImage profilePicLoad(String email);
+}
