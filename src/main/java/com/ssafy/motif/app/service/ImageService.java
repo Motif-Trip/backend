a36ec8bbeda44ec5f7c49029a9b79dba48d7f487
azin Lee <nuestiloveyou@gmail.com>
2023-11-21 11:29:28 +0900

    :sparkles: Image service developed

--- src/main/java/com/ssafy/motif/app/service/ImageService.java
+++ src/main/java/com/ssafy/motif/app/service/ImageService.java
@@ -1,7 +1,10 @@
 package com.ssafy.motif.app.service;
 
+import java.awt.Image;
+
 import org.springframework.web.multipart.MultipartFile;
 
 public interface ImageService {
-	String profilePicAdd(MultipartFile file, Long memberId);
+	String profilePicAdd(MultipartFile file, String email);
+	Image profilePicLoad(String email);
 }
