47f6846d1555c969b2b437f6b7c4438c27c369c4
Thingk0 <audtjd9080@naver.com>
2023-11-24 00:07:54 +0900

    :necktie: 스케줄 생성 로직 개선 및 타임테이블 ID 연동 추가
    - [X] 시작 및 종료 시간에 대한 유효성 검증 로직 추가
    - [X] 로그인한 사용자의 타임테이블 ID 조회 기능 추가
    - [X] 타임테이블 ID가 없는 경우 커스텀 예외 발생

--- src/main/java/com/ssafy/motif/app/service/impl/ScheduleServiceImpl.java
+++ src/main/java/com/ssafy/motif/app/service/impl/ScheduleServiceImpl.java
@@ -1,7 +1,10 @@
 package com.ssafy.motif.app.service.impl;
 
+import com.ssafy.motif.app.code.ErrorCode;
 import com.ssafy.motif.app.domain.dto.schedule.ScheduleCreateRequestDto;
 import com.ssafy.motif.app.domain.mapper.ScheduleMapper;
+import com.ssafy.motif.app.domain.mapper.TimetableMapper;
+import com.ssafy.motif.app.exception.NotFoundTimetableException;
 import com.ssafy.motif.app.service.ScheduleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,10 +13,23 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class ScheduleServiceImpl implements ScheduleService {
 
+    private final TimetableMapper timetableMapper;
     private final ScheduleMapper mapper;
 
     @Override
-    public void create(ScheduleCreateRequestDto requestDto, String loggedInEmail) {
-        mapper.save(requestDto, loggedInEmail);
+    public Long create(ScheduleCreateRequestDto requestDto, String loggedInEmail) {
+
+        /* 시작 시간 및 종료 시간 유효성 검증*/
+        requestDto.validateTime();
+
+        /* 타임테이블 ID 조회 */
+        Long timetableId = timetableMapper.getTimetableIdByEmail(loggedInEmail);
+        if (timetableId == null) {
+            throw new NotFoundTimetableException(ErrorCode.TIMETABLE_NOT_FOUND);
+        }
+
+        /* 타임테이블 저장 */
+        mapper.save(timetableId, requestDto, loggedInEmail);
+        return requestDto.getScheduleId();
     }
 }
