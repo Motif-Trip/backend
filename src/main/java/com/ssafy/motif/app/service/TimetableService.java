bde55d21d28355280232f1237cc9cc8de02dbd81
Thingk0 <audtjd9080@naver.com>
2023-11-25 20:29:58 +0900

    :necktie: 날짜 -> 회원 기반 조회 로직으로 변경
    - [X] 'save' 메소드 수정: 시간표를 저장하는 로직에서 날짜를 기반으로 중복을 체크하는 부분을 제거하고, 회원 ID만으로 시간표 중복을 체크하는 방식으로 수정.
    - [X] 'getSchedulesTableByDate' 메소드 수정: 특정 날짜에 해당하는 시간표를 조회하는 로직에서 회원 ID를 기반으로 시간표를 조회하는 로직으로 변경.
     - 해당 회원의 시간표가 존재하지 않을 경우 'NotFoundTimetableException' 예외를 발생시키도록 수정.

--- src/main/java/com/ssafy/motif/app/service/TimetableService.java
+++ src/main/java/com/ssafy/motif/app/service/TimetableService.java
@@ -2,12 +2,11 @@ package com.ssafy.motif.app.service;
 
 import com.ssafy.motif.app.domain.dto.schedule.ScheduleResponseDto;
 import com.ssafy.motif.app.domain.dto.timetable.TimetableCreateRequestDto;
-import java.time.LocalDateTime;
 import java.util.List;
 
 public interface TimetableService {
 
     Long save(TimetableCreateRequestDto requestDto, String email);
 
-    List<ScheduleResponseDto> getSchedulesTableByDate(LocalDateTime dateTime, String email);
+    List<ScheduleResponseDto> getSchedulesTableByDate(String email);
 }
--- src/main/java/com/ssafy/motif/app/service/impl/TimetableServiceImpl.java
+++ src/main/java/com/ssafy/motif/app/service/impl/TimetableServiceImpl.java
@@ -7,13 +7,12 @@ import com.ssafy.motif.app.domain.dto.timetable.TimetableCreateRequestDto;
 import com.ssafy.motif.app.domain.mapper.MemberMapper;
 import com.ssafy.motif.app.domain.mapper.ScheduleMapper;
 import com.ssafy.motif.app.domain.mapper.TimetableMapper;
+import com.ssafy.motif.app.exception.NotFoundTimetableException;
 import com.ssafy.motif.app.exception.TimetableDeduplicationException;
 import com.ssafy.motif.app.exception.member.NotFoundMemberException;
 import com.ssafy.motif.app.service.TimetableService;
-import java.time.LocalDateTime;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -29,7 +28,7 @@ public class TimetableServiceImpl implements TimetableService {
     @Override
     public Long save(TimetableCreateRequestDto requestDto, String email) {
         LoginResponseDto loginInfo = getLoginResponseDto(email);
-        if (timetableMapper.getTimetableId(loginInfo.getMemberId(), requestDto.getDateTime()) != null) {
+        if (timetableMapper.getTimetableId(loginInfo.getMemberId()) != null) {
             throw new TimetableDeduplicationException(ErrorCode.TIMETABLE_DUPLICATE);
         }
 
@@ -39,9 +38,15 @@ public class TimetableServiceImpl implements TimetableService {
 
     @Override
     @Transactional(readOnly = true)
-    public List<ScheduleResponseDto> getSchedulesTableByDate(@Nullable LocalDateTime dateTime, String email) {
+    public List<ScheduleResponseDto> getSchedulesTableByDate(String email) {
+
         LoginResponseDto loginInfo = getLoginResponseDto(email);
-        Long timetableId = timetableMapper.getTimetableId(loginInfo.getMemberId(), dateTime);
+        Long timetableId = timetableMapper.getTimetableId(loginInfo.getMemberId());
+
+        if (timetableId == null) {
+            throw new NotFoundTimetableException(ErrorCode.TIMETABLE_NOT_FOUND);
+        }
+
         return scheduleMapper.fetchItemByTimetableId(timetableId);
     }
 
