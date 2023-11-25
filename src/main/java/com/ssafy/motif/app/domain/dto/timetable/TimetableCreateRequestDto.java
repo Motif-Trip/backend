85960ae3c87ea99c1d60314458a5da085c94b9b8
Thingk0 <audtjd9080@naver.com>
2023-11-25 20:33:36 +0900

    :recycle: 여행 날짜 관련 필드 제거
    - [X] 'dateTime' 필드 제거: 사용자가 시간표를 생성할 때, 특정 날짜를 지정하지 않고 시간표 이름만으로 생성할 수 있게 수정

--- src/main/java/com/ssafy/motif/app/domain/dto/timetable/TimetableCreateRequestDto.java
+++ src/main/java/com/ssafy/motif/app/domain/dto/timetable/TimetableCreateRequestDto.java
@@ -2,8 +2,6 @@ package com.ssafy.motif.app.domain.dto.timetable;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import io.swagger.v3.oas.annotations.media.Schema;
-import java.time.LocalDateTime;
-import javax.validation.constraints.FutureOrPresent;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 import lombok.AccessLevel;
@@ -28,9 +26,5 @@ public class TimetableCreateRequestDto {
     @Size(max = 100, message = "테이블 이름은 최대 100자 이내여야 합니다.")
     @Schema(description = "테이블 - 여행계획표 이름", example = "건대역 맛집 탐방")
     private String tableName;
-
-    @FutureOrPresent(message = "여행 날짜는 현재 또는 미래의 날짜여야 합니다.")
-    @Schema(description = "여행날짜", example = "2023-01-01T08:00:00")
-    private LocalDateTime dateTime;
 
 }
